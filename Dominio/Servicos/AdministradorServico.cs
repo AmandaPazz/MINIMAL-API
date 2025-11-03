60a7ab0d0dd7f6a68fb86c07cc3614d443a78641
Amanda Paz <130465080+AmandaPazz@users.noreply.github.com>
2025-11-03 18:48:19 -0300

    feat: configurações iniciais + criando endpoints para administrador

---
+++ Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
+namespace MinimalApi.Dominio.Servicos;
+
+public class AdministradorServico : IAdministradorServico
+{
+
+
+    public readonly DbContexto _contexto;
+
+    public AdministradorServico(DbContexto contexto){
+        _contexto = contexto;
+    }
+
+    public Administrador? Incluir(Administrador administrador)
+    {
+        _contexto.Administradores.Add(administrador);
+        _contexto.SaveChanges();
+
+        return administrador;
+    }
+
+    public Administrador? Login(LoginDTO loginDTO)
+    {
+        return _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
+    }
+
+
+    public List<Administrador> Todos(int? pagina)
+    {
+        var query = _contexto.Administradores.AsQueryable();
+
+        int intensPorPagina = 10;
+
+        if (pagina != null)
+        {
+            query = query.Skip(((int)pagina - 1) * intensPorPagina).Take(intensPorPagina);
+        }
+
+        return query.ToList();
+    }
+
+
+     public Administrador? BuscaPorId(int id)
+    {
+        return _contexto.Administradores.Where(v => v.Id == id).FirstOrDefault();
+    }
+}
